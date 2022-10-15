cc412e571e695cf54dfa0789a7b1302b319b4883
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2022-10-15 10:57:27 -0700

    commit horrible crimes against the code
    it is now functional on updated discord.js

--- extension/commands.js
+++ extension/commands.js
@@ -1,13 +1,15 @@
+const {PermissionsBitField} = require('discord.js');
 module.exports = (client, alias, roleID, callback) => {
-	client.on('message', (message) => {
+	client.on('messageCreate', (message) => {
 		const { content } = message;
 		const command = `<@!${client.user.id}> ${alias}`;
+		const command1 = `<@${client.user.id}> ${alias}`;
 
-		if (content === command) {
-			if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_CHANNELS') || message.member.roles.cache.has(roleID))
+		if (content === command || content === command1) {
+			if (message.member.permissions.has(PermissionsBitField.Flags.Administrator) || message.member.permissions.has(PermissionsBitField.Flags.ManageChannels) || message.member.roles.cache.has(roleID))
 				callback(message)
 			else
-				message.reply(`you do not have permission to execute this command!`)
+				message.reply(`You do not have permission to execute this command!`)
 		}
 	})
 }
--- extension/index.js
+++ extension/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
-const Discord = require('discord.js');
+const {Client, IntentsBitField} = require('discord.js');
+const {joinVoiceChannel} = require('@discordjs/voice');
 const command = require('./commands');
 
-let silence, connection;
+let connection, speakingMap, channel;
 
 module.exports = function (nodecg) {
 
@@ -17,10 +18,77 @@ module.exports = function (nodecg) {
 	speaking.value = null;
 	memberList.value = [];
 	connection = undefined;
+	speakingMap = undefined;
+	channel = undefined;
 
 	let roleID = nodecg.bundleConfig.roleID;
 
-	const client = new Discord.Client();
+	const intents = new IntentsBitField();
+	intents.add(IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildVoiceStates);
+	const client = new Client({ intents: intents });
+
+	command(client, 'connect', roleID, (message) => {
+		if (connection === undefined && message.member.voice.channel !== undefined && message.member.voice.channel !== null)
+			record(message.member.voice.channel);
+		else
+			message.reply(`You're not in a voice channel!`)		
+	});
+
+	command(client, 'disconnect', roleID, (message) => {
+		if (connection !== undefined)
+			stopRecording();
+		else
+			message.reply(`I'm not in a voice channel!`)
+	});
+
+	client.on('voiceStateUpdate', (oldMember, newMember) => {
+		if (connection !== undefined && newMember.id !== client.user.id) {
+			if (oldMember.channelID !== channel.id && newMember.channelID === channel.id) {
+				let muteState;
+				if (newMember.selfMute || newMember.selfDeaf || newMember.serverMute || newMember.serverDeaf)
+					muteState = true;
+				else
+					muteState = false;
+				addMember.value = null;
+				addMember.value = { id: newMember.id, name: newMember.member.user.username, avatar: newMember.member.user.displayAvatarURL(), muted: muteState };
+				memberList.value.push({ id: newMember.id, name: newMember.member.user.username, avatar: newMember.member.user.displayAvatarURL(), muted: muteState });
+			} else if (oldMember.channelID === channel.id && newMember.channelID !== channel.id) {
+				for (let i = 0; i < memberList.value.length; i++) {
+					if (memberList.value[i].id === newMember.id) {
+						memberList.value.splice(i, 1)
+						break;
+					}
+				}
+				removeMember.value = null;
+				removeMember.value = newMember.id;
+			} else if (newMember.serverMute !== oldMember.serverMute || newMember.serverDeaf !== oldMember.serverDeaf || newMember.selfMute !== oldMember.selfMute || newMember.selfDeaf !== oldMember.selfDeaf) {
+				for (let i = 0; i < memberList.value.length; i++) {
+					if (memberList.value[i].id === newMember.id) {
+						let muteState;
+						if (newMember.serverMute || newMember.serverDeaf || newMember.selfMute || newMember.selfDeaf)
+							muteState = true;
+						else
+							muteState = false;
+						changeMute.value = null;
+						changeMute.value = { id: newMember.id, muted: muteState }
+						memberList.value[i].muted = muteState;
+					}
+				}
+			}
+		} else if (connection !== undefined && oldMember.channelID !== null) {
+			stopRecording();
+			if (newMember.channelID !== null && newMember.channelId !== channel.id) {
+				// i'm too stupid to figure out how to do this properly so here we are
+				client.channels.fetch(newMember.channelId).then(c => channel = c);
+				setTimeout(function () {
+					record(channel); 
+				}, 500);
+			}
+		}
+	});	
+
+
+
 	client.once('ready', () => {
 		nodecg.log.info('DACBot is now online. For help, type @' + client.user.username + ' help')
 
@@ -31,78 +99,30 @@ module.exports = function (nodecg) {
 			else
 				client.user.setActivity(newVal.length + ' users...', { type: "LISTENING" });
 		});
-
-		command(client, 'connect', roleID, (message) => {
-			if (connection === undefined && message.member.voice.channel !== undefined && message.member.voice.channel !== null)
-				record(message.member.voice.channel.id);
-			else
-				message.reply(`you're not in a voice channel!`)
-		})
-
-		command(client, 'disconnect', roleID, (message) => {
-			if (connection !== undefined)
-				stopRecording(connection.channel.name);
-			else
-				message.reply(`I'm not in a voice channel!`)
-		});
-
-		client.on('guildMemberSpeaking', (member, memberSpeaking) => {
-			if (memberSpeaking.bitfield == 1)
-				speakState = true;
-			else
-				speakState = false;
-			speaking.value = null;
-			speaking.value = { id: member.id, speaking: speakState }
-		});
-
-		client.on('voiceStateUpdate', (oldMember, newMember) => {
-			if (connection !== undefined && newMember.id !== client.user.id) {
-				if (oldMember.channelID !== connection.channel.id && newMember.channelID === connection.channel.id) {
-					let muteState;
-					if (newMember.selfMute || newMember.selfDeaf || newMember.serverMute || newMember.serverDeaf)
-						muteState = true;
-					else
-						muteState = false;
-					addMember.value = null;
-					addMember.value = { id: newMember.id, name: newMember.member.user.username, avatar: newMember.member.user.displayAvatarURL(), muted: muteState };
-					memberList.value.push({ id: newMember.id, name: newMember.member.user.username, avatar: newMember.member.user.displayAvatarURL(), muted: muteState });
-				}
-				else if (oldMember.channelID === connection.channel.id && newMember.channelID !== connection.channel.id) {
-					for (let i = 0; i < memberList.value.length; i++) {
-						if (memberList.value[i].id === newMember.id) {
-							memberList.value.splice(i, 1)
-							break;
-						}
-					}
-					removeMember.value = null;
-					removeMember.value = newMember.id;
-				}
-				else if (newMember.serverMute !== oldMember.serverMute || newMember.serverDeaf !== oldMember.serverDeaf || newMember.selfMute !== oldMember.selfMute || newMember.selfDeaf !== oldMember.selfDeaf) {
-					for (let i = 0; i < memberList.value.length; i++) {
-						if (memberList.value[i].id === newMember.id) {
-							let muteState;
-							if (newMember.serverMute || newMember.serverDeaf || newMember.selfMute || newMember.selfDeaf)
-								muteState = true;
-							else
-								muteState = false;
-							changeMute.value = null;
-							changeMute.value = { id: newMember.id, muted: muteState }
-							memberList.value[i].muted = muteState;
-						}
-					}
-				}
-			}
-			else if (connection !== undefined && oldMember.channelID !== null) {
-				stopRecording(oldMember.channel.name);
-				if (newMember.channelID !== null && newMember.channelID !== connection.channel.id)
-					setTimeout(function () { record(newMember.channelID); }, 500);
-			}
-		})
 	});
-	async function record(channelID) {
-		connection = await client.channels.cache.get(channelID).join();
 
-		client.channels.cache.get(channelID).members.forEach((member) => {
+
+
+	function record(chan) {
+		connection = joinVoiceChannel({
+			channelId: chan.id, 
+			guildId: chan.guild.id,
+			adapterCreator: chan.guild.voiceAdapterCreator,
+			selfDeaf: false,
+			selfMute: true
+		});
+		speakingMap = connection.receiver.speaking;
+		channel = chan;
+		speakingMap.on('start', (userID) => {
+			speaking.value = null;
+			speaking.value = { id: userID, speaking: true }
+		});
+		speakingMap.on('end', (userID) => {
+			speaking.value = null;
+			speaking.value = { id: userID, speaking: false }
+		});
+
+		client.channels.cache.get(channel.id).members.forEach((member) => {
 			if (member.user.id !== client.user.id) {
 				let muteState;
 				if (member.voice.selfMute || member.voice.selfDeaf || member.voice.serverMute || member.voice.serverDeaf)
@@ -115,24 +135,22 @@ module.exports = function (nodecg) {
 			}
 		})
 
-		silence = setInterval(function () {
-			connection.play(fs.createReadStream('./bundles/nodecg-dacbot/sounds/silence.wav'));
-		}, 270000)
 		nodecg.log.info('Capture started for channel ' + connection.channel.name + ' on ' + Date());
 	}
 
-	function stopRecording(channelName) {
+	function stopRecording() {
 		if (connection !== undefined) {
-			nodecg.log.info('Capture stopped for channel ' + channelName + ' on ' + Date())
-			connection.channel.leave();
+			nodecg.log.info('Capture stopped for channel ' + channel.name + ' on ' + Date())
+			connection.destroy();
 		}
 		for (let i = 0; i < memberList.value.length; i++) {
 			removeMember.value = null;
 			removeMember.value = memberList.value[i].id;
 		}
 		memberList.value = [];
+		speakingMap = undefined;
 		connection = undefined;
-		clearInterval(silence);
+		channel = undefined;
 	}
 	client.login(nodecg.bundleConfig.botToken);
 };
